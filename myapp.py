dbed93467d4bae2d82bb78acfbb03444cd83952c
Arun Regmi <aregmi450@gmail.com>
2023-04-27 12:43:19 +0545

    adding new file for streamlit visualization

---
+++ myapp.py
@@ -0,0 +1,74 @@
+# Importing required modules 
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+sns.set()
+plt.style.use('seaborn-whitegrid')
+## Reading the dataset and looking the information on the dataset
+npr = pd.read_csv('Conversion.csv')
+npr.head()
+#Finding out the information about dataset data
+npr.info();
+#converting the date column to datetime format 
+npr['Date'] = pd.to_datetime(npr['Date'])
+print(f'This dataset contains exchange rates of USD in Nepal from {npr.Date.min()} {npr.Date.max()}')
+print(f'Total Days = {(npr.Date.max() - npr.Date.min()).days} days')
+npr.describe()
+## Plot showing rate of change of exchange rate 
+plt.figure(figsize=(10, 4))
+plt.title("NPR - USD Exchange Rate")
+plt.xlabel('Date')
+plt.ylabel("Close")
+plt.plot(npr['Date'], npr['Close'])
+plt.show()
+### Using Linear Regression Model to Predict
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+#splitting data into train and test sets 
+
+x = np.array(npr.index).reshape(-1,1)
+y =npr['Close']
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=42)
+#Feature Scaling
+scaler = StandardScaler().fit(xtrain)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
+lm = LinearRegression()
+lm.fit(xtrain, ytrain)
+# Use model to make predictions
+y_pred = lm.predict(xtest)
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+
+# Printout relevant metrics
+print("Model Coefficients:", lm.coef_)
+print("Mean Absolute Error:", mean_absolute_error(ytest, y_pred))
+print("Coefficient of Determination:", r2_score(ytest, y_pred))
+## Using LSTM Model to Predict 
+x = npr[["Open", "High", "Low", "Volume"]]
+y = npr["Close"]
+x = x.to_numpy()
+y = y.to_numpy()
+y = y.reshape(-1, 1)
+
+from sklearn.model_selection import train_test_split
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=42)
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+model = Sequential()
+model.add(LSTM(128, return_sequences=True, input_shape= (xtrain.shape[1], 1)))
+model.add(LSTM(64, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+model.summary()
+### Training the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(xtrain, ytrain, batch_size=1, epochs=50);
+npr.tail(1)
+import numpy as np
+#features = [Open, High, Low, Adj Close, Volume]
+features = np.array([[127.867599, 127.867599, 127.867599, 0]])
+model.predict(features)
